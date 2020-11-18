c6e466763ea719986ac33bfef25d82fb8377eb97
leonardocoll <leonardocoll@users.noreply.github.com>
2020-11-17 18:08:43 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-//setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
-// setup nav
+// // setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
@@ -14,3 +10,5 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
+//setup date
+const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
